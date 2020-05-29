8a31840639aba5e16c8bd49637be65b0910273ab
hawrot <mhawro200@caledonian.ac.uk>
2020-05-29 22:33:26 +0100

    Add dummy ingredients into the recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,25 @@
 import {Recipe} from "./recipe.model";
 import {EventEmitter} from "@angular/core";
+import {Ingredient} from "../shared/ingredient.model";
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-        new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+        new Recipe('Polish Dumplings', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+            [
+                new Ingredient('Cheese', 2),
+                new Ingredient('Flour Plain', 1),
+            ]),
+        new Recipe('Croquetas', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+            [
+                new Ingredient('Serrano Ham', 1),
+                new Ingredient('Milk', 10),
+
+            ])
     ];
 
-    getRecipes(){
+    getRecipes() {
         return this.recipes.slice(); //return a copy
     }
 }
