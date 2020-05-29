87c5898cb591b52e2b87f35441d48cfae05b450d
hawrot <mhawro200@caledonian.ac.uk>
2020-05-29 22:49:03 +0100

    Push ingredients from recipe into shopping list

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Recipe } from './recipe.model';
-import {RecipeService} from "./recipe.service";
+import { RecipeService } from './recipe.service';
 
 @Component({
   selector: 'app-recipes',
@@ -10,17 +10,17 @@ import {RecipeService} from "./recipe.service";
   providers: [RecipeService]
 })
 export class RecipesComponent implements OnInit {
-
   selectedRecipe: Recipe;
 
   constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
-    this.recipeService.recipeSelected.subscribe(
-        (recipe: Recipe) => {
-          this.selectedRecipe = recipe;
-        }
-    );
+    this.recipeService.recipeSelected
+        .subscribe(
+            (recipe: Recipe) => {
+              this.selectedRecipe = recipe;
+            }
+        );
   }
 
 }
