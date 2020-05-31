ede8fea65050206610ed192c69987d2179d514af
hawrot <mhawro200@caledonian.ac.uk>
2020-05-31 23:12:29 +0100

    Delete an item from the shopping list

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -60,4 +60,9 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
      this.editMode = false;
     }
 
+    onDelete(){
+        this.shoppingListService.deleteIngredient(this.editedItemIndex);
+        this.onClean();
+    }
+
 }
