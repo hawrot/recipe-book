eb12d56bbc6eb0ac88e16751dc7c46b07a809129
hawrot <mhawro200@caledonian.ac.uk>
2020-05-31 23:49:13 +0100

    Add extra ingredient while editing recipe

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -60,6 +60,15 @@ export class RecipeEditComponent implements OnInit {
       });
   }
 
+  onAddIngredient(){
+      (<FormArray> this.recipeForm.get('ingredients')).push(
+          new FormGroup({
+              'name': new FormControl(),
+              'amount': new FormControl(),
+          })
+      );
+  }
+
     get controls() { // a getter!
         return (<FormArray>this.recipeForm.get('ingredients')).controls;
     }
