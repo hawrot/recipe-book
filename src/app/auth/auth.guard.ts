9beb548e3135b0585ee87eccd77988d0bd88f9e3
hawrot <mhawro200@caledonian.ac.uk>
2020-06-02 22:41:47 +0100

    Redirect to /auth if not authenticated using UrlTree

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,4 +1,4 @@
-import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree} from "@angular/router";
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from "@angular/router";
 import {Injectable} from "@angular/core";
 import {AuthService} from "./auth.service";
 import {map} from "rxjs/operators";
@@ -7,12 +7,16 @@ import {Observable} from "rxjs";
 @Injectable({providedIn: 'root'})
 export class AuthGuard implements CanActivate {
 
-    constructor(private authService: AuthService) {
+    constructor(private authService: AuthService, private router : Router) {
     }
 
     canActivate(route: ActivatedRouteSnapshot, router: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
         return this.authService.user.pipe(map(user => {
-            return !!user
+            const isAuth = !!user
+            if(isAuth){
+                return true;
+            }
+            return this.router.createUrlTree(['/auth']);
         }));
     }
 
