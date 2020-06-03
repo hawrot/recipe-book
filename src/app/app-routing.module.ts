dbc085ebbe6a0cdb55157a3c697988b26ff53daa
hawrot <mhawro200@caledonian.ac.uk>
2020-06-03 22:47:57 +0100

    Add lazyLoading for recipes component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import {Routes, RouterModule} from '@angular/router';
 
 const appRoutes: Routes = [
     {path: '', redirectTo: '/recipes', pathMatch: 'full'},
+    {path: 'recipes', loadChildren:  () => import('./recipes/recipes.module').then(module => module.RecipesModule ) }
 ];
 
 @NgModule({
