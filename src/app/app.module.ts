956695be7f80aad0b7675e6921cc7129557f5cbd
hawrot <mhawro200@caledonian.ac.uk>
2020-06-03 21:35:22 +0100

    The ShoppingList Feature Model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
-import {ShoppingListComponent} from './shopping-list/shopping-list.component';
-import {ShoppingEditComponent} from './shopping-list/shopping-edit/shopping-edit.component';
 import {DropdownDirective} from './shared/dropdown.directive';
 import {ShoppingListService} from './shopping-list/shopping-list.service';
 import {AppRoutingModule} from './app-routing.module';
@@ -15,13 +13,12 @@ import {LoadingSpinnerComponent} from "./shared/loading-spinner/loading-spinner.
 import {AuthInterceptorService} from "./auth/auth-interceptor.service";
 import {AlertComponent} from "./shared/alert/alert.component";
 import {RecipesModule} from "./recipes/recipes.module";
+import {ShoppingListModule} from "./shopping-list/shopping-list.module";
 
 @NgModule({
     declarations: [
         AppComponent,
         HeaderComponent,
-        ShoppingListComponent,
-        ShoppingEditComponent,
         DropdownDirective,
         AuthComponent,
         LoadingSpinnerComponent,
@@ -33,7 +30,8 @@ import {RecipesModule} from "./recipes/recipes.module";
         ReactiveFormsModule,
         HttpClientModule,
         AppRoutingModule,
-        RecipesModule
+        RecipesModule,
+        ShoppingListModule
     ],
     providers: [ShoppingListService, RecipeService, {
         provide: HTTP_INTERCEPTORS,
